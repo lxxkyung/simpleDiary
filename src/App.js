d5fd139e27becbc4328995f0c0bee85f9445778c
2kyunghee <lxxkyung@gmail.com>
2023-11-21 23:02:49 +0900

    심플 다이어리

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import DiaryEditor from './DiaryEdior';
+
+
+function App() {
+  return (
+    <div className="App">
+        <DiaryEditor/>
+    </div>
+  );
+}
+
+export default App;
